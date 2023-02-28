c2b8e885d515a880f28a9f16384486e2a7e0212e
sunxiaoou <sun_xiaoou@qq.com>
2023-02-28 12:36:08 +0800

    add Parser constructor

--- src/main/java/lambdasinaction/tmp/Parser.java
+++ src/main/java/lambdasinaction/tmp/Parser.java
@@ -5,56 +5,79 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 public class Parser {
-    public static void main(String[] args) throws IOException, SQLException {
-        String path = "target/classes/lambdasinaction/tmp/schemaMap_policy.json";
+    private final JSONObject obj;
+
+    public Parser(String path) throws FileNotFoundException {
         BufferedReader br = new BufferedReader(new FileReader(path));
         String jsonString = br.lines().collect(Collectors.joining("\n"));
+        obj = JSON.parseObject(jsonString);
+    }
 
-        JSONObject jsonObject = JSON.parseObject(jsonString);
-        boolean entireDbSync = jsonObject.getBoolean("entireDbSync");
+    public Boolean isEntire() {
+        return obj.getBoolean("entireDbSync");
+    }
+
+    public List<String> getSchemas() {
         Map<String, String> schemaMap =
-                JSON.parseObject(jsonObject.getJSONObject("schemaMap").toJSONString(), Map.class);
-        List<String> schemas = new ArrayList<>(schemaMap.keySet());
-        JSONArray tableMaps = jsonObject.getJSONArray("tableMap");
+                JSON.parseObject(obj.getJSONObject("schemaMap").toJSONString(), Map.class);
+        return new ArrayList<>(schemaMap.keySet());
+    }
 
+    public List<String> getTables() {
+        JSONArray tableMaps = obj.getJSONArray("tableMap");
         List<String> tables = new ArrayList<>();
         for (Object tableMap: tableMaps) {
             String table = ((JSONObject)tableMap).getString("srcTable");
             String schema = ((JSONObject)tableMap).getString("srcSchema");
             tables.add(schema + "." + table);
         }
-        System.out.println(entireDbSync);
-        System.out.println(schemas);
-        System.out.println(tables);
+        return tables;
+    }
 
-        MyJDBC jdbc = new MyJDBC("localhost", "", "manga", "manga");
-        List<String> tables2Sync = new ArrayList<>();
-        List<String> schemaList = jdbc.schemaList();
-        List<String> unknownSchema = new ArrayList<>();
-        List<String> unknownTables = new ArrayList<>();
+    public static void main(String[] args) throws IOException, SQLException {
+        String path = "target/classes/lambdasinaction/tmp/schemaMap_policy.json";
+        Parser parser = new Parser(path);
 
+        System.out.println("entireDbSync - " + parser.isEntire());
+        List<String> jsonSchemas = parser.getSchemas();
+        System.out.println("jsonSchemas - " + jsonSchemas);
+        List<String> jsonTables = parser.getTables();
+        System.out.println("jsonTables - " + jsonTables);
+
+        MyJDBC db = new MyJDBC("localhost", "", "manga", "manga");
+        List<String> dbSchemas = db.schemaList();
+        System.out.println("dbSchemas - " + dbSchemas);
+        List<String> unknownSchema = jsonSchemas.stream()
+                .filter(s -> ! dbSchemas.contains(s))
+                .collect(Collectors.toList());
+        System.out.println("unknownSchema - " + unknownSchema);
         List<String> exclusive = new ArrayList<>(Arrays.asList("information_schema", "metastore"));
 
-
-        for (String schema: schemaList) {
+        List<String> tables = new ArrayList<>();
+        for (String schema: dbSchemas) {
             if (! exclusive.contains(schema)) {
-                List<String> tabList = jdbc.tabList(schema);
-                for (String tab : tabList) {
+                List<String> dbTables = db.tabList(schema);
+                for (String tab : dbTables) {
                     String table = schema + "." + tab;
-                    if (entireDbSync || schemas.contains(schema) || tables.contains(table)) {
-                        tables2Sync.add(table);
+                    if (parser.isEntire() || jsonSchemas.contains(schema) || jsonTables.contains(table)) {
+                        tables.add(schema + "." + tab);
                     }
                 }
             }
         }
-        System.out.println(tables2Sync);
-        jdbc.close();
+        System.out.println("tables - " + tables);
+        List<String> unknownTables = jsonTables.stream().filter(s -> ! tables.contains(s)).collect(Collectors.toList());
+        System.out.println("unknownTables - " + unknownTables);
+
+        db.close();
     }
 }
